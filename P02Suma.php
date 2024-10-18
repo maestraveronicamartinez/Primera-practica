fd1825327da58fcebfe0eda92717522a3f303146
maestraveronicamartinez <veronicamartinez@cbtis89.edu.mx>
2024-10-17 19:40:29 -0600

    Add files via upload

---
+++ P02Suma.php
@@ -0,0 +1,19 @@
+<?php
+/* CBTIS 89 
+   Programa que suma dos números enteros
+   Verónica Martínez Anaya
+   3ºA Programación Matutino
+*/
+
+$N1;$N2;$Suma; //se declaran las variables.
+$N1=10; //se asigna valor a N1
+$N2=7; //se asigna valor a N2
+$Suma=$N1+$N2;  //se realiza la operación aritmética.
+
+// En las siguientes líneas se imprimen los valores de las variables y el resultado de la suma.
+echo "El primer número es: ", $N1;
+echo "<br>"; //salto de línea
+echo "El segundo número es: ", $N2;
+echo "<br>";  //salto de línea
+echo "La suma de los números es: ", $Suma;
+?>
